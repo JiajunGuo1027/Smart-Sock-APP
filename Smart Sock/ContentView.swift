b3f30a4b5f02c7640738cf38947f5203f98ecd41
JiajunGuo1027 <jiajunguo1027@gmail.com>
2024-04-17 16:02:52 +0100

    The first submission contains all the contents of the Smart Sock APP folder.

--- Smart Sock/ContentView.swift
+++ Smart Sock/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Smart Sock
 //
-//  Created by 郭家骏 on 14/03/2024.
+//  Created by 郭家骏 on 07/03/2024.
 //
 
 import SwiftUI
@@ -10,24 +10,82 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    @State private var fsrThreshold: Float = 50.0 // Default value, mid-range
+    @State private var flexThreshold: Float = -5 // Default value, mid-range
+    @State private var showAlert = false
+    @State private var alertMessage = ""
+    @StateObject private var bluetoothManager = BluetoothManager()
+    
+    // Add an initializer. And set the "onThresholdExceeded" closure for "BluetoothManager".
+    init() {
+        _bluetoothManager = StateObject(wrappedValue: BluetoothManager())
+    }
 
+    // The FetchRequest for SensorDataEntity
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \SensorDataEntity.timestamp, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var sensorDataItems: FetchedResults<SensorDataEntity>
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+              List {
+                  // FSR Threshold setting section
+                  Section(header: Text("FSR Threshold")) {
+                      HStack {
+                          Slider(value: $fsrThreshold, in: 0...100, step: 1)
+                              .onChange(of: fsrThreshold) { newValue, _ in
+                                  bluetoothManager.fsrThreshold = newValue
+                              }
+                          TextField("Threshold", value: $fsrThreshold, formatter: NumberFormatter())
+                              .frame(width: 50)
+                              .textFieldStyle(.roundedBorder)
+                              .onChange(of: fsrThreshold) { newValue, _ in
+                                  bluetoothManager.fsrThreshold = newValue
+                              }
+                      }
+                  }
+                  // Flex Threshold setting section
+                  Section(header: Text("Flex Threshold")) {
+                      HStack {
+                          Slider(value: $flexThreshold, in: -10...0, step: 0.1)
+                              .onChange(of: flexThreshold) { newValue, _ in
+                                  bluetoothManager.flexThreshold = newValue
+                              }
+                          TextField("Threshold", value: $flexThreshold, formatter: NumberFormatter())
+                              .frame(width: 50)
+                              .textFieldStyle(.roundedBorder)
+                              .onChange(of: flexThreshold) { newValue, _ in
+                                  bluetoothManager.flexThreshold = newValue
+                              }
+                      }
+                  }
+                  
+                // Anklet and wristband data display section
+                Section(header: Text(bluetoothManager.statusMessage)) {
+                    if bluetoothManager.isConnected {
+                        //Display the names of the connected devices
+                        ForEach(bluetoothManager.connectedDevices, id: \.self) { deviceName in
+                            Text(deviceName)
+                        }
+                        // Display all data
+                        ForEach(sensorDataItems) { data in
+                            VStack(alignment: .leading) {
+                               Text("FSR Change: \(data.fsrChange)")
+                               Text("Flex Change: \(data.flexChange)")
+                               Text("Heart Rate: \(data.heartRate)bpm")
+                               Text("SpO2: \(data.spo2)%")
+                               Text("Timestamp: \(data.timestamp.map { itemFormatter.string(from: $0) } ?? "N/A")")
+                            }
                 }
                 .onDelete(perform: deleteItems)
+            } else {
+                // A message is displayed when the device is not connected or searched.
+                Text("Make sure your Bluetooth device is on and in range.")
             }
+        }
+    }
+    .navigationTitle("Smart Sock")
             .toolbar {
 #if os(iOS)
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -35,40 +93,55 @@ struct ContentView: View {
                 }
 #endif
                 ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    Button(action: addData) {
+                        Label("Add Sensor Data", systemImage: "plus")
                     }
                 }
             }
-            Text("Select an item")
+            // Show Threshold
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Threshold"), message: Text(alertMessage), dismissButton: .default(Text("Confirm")))
+            }
+        }
+        .onAppear {
+            bluetoothManager.fsrThreshold = fsrThreshold
+            bluetoothManager.flexThreshold = flexThreshold
+            bluetoothManager.onThresholdExceeded = { sensorType in
+                DispatchQueue.main.async {
+                    self.alertMessage = "\(sensorType) sensor detected swelling."
+                    self.showAlert = true
+                    UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+                }
+            }
         }
     }
-
-    private func addItem() {
+    
+    // Load latest data
+    private func addData() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
+            let newData = SensorDataEntity(context: viewContext)
+            newData.fsrChange = bluetoothManager.lastFsrChange
+            newData.flexChange = bluetoothManager.lastFlexChange
+            // heartRate and spo2 are stored as Int16 in Core Data, so convert these values accordingly
+            newData.heartRate = Int16(bluetoothManager.lastHeartRate)
+            newData.spo2 = Int16(bluetoothManager.lastSpo2)
+            newData.timestamp = Date()
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
-
+    // Delete data
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
+            offsets.map { sensorDataItems[$0] }.forEach(viewContext.delete)
 
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -83,6 +156,8 @@ private let itemFormatter: DateFormatter = {
     return formatter
 }()
 
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
 }
